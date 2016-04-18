010220bbebb7dc0381e16f3ee241725e2407d7cb
Dennis Wollersheim <dewoller@gmail.com>
2016-04-18 22:13:07 +1000

    Music module tested working

--- pi_dispatcher/Music.py
+++ pi_dispatcher/Music.py
@@ -17,6 +17,14 @@ import const
 import thread, sys
 from i2clibraries import i2c_lcd_smbus
 
+try:
+    import RPi.GPIO as GPIO
+except RuntimeError:
+    import GPIO as GPIO
+    logger.info("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
+
+GPIO.setmode(GPIO.BOARD)
+
 songs = [
 	([392,294,0,392,294,0,392,0,392,392,392,0,1047,262], [0.2,0.2,0.2,0.2,0.2,0.2,0.1,0.1,0.1,0.1,0.1,0.1,0.8,0.4]),
 	([262,330,392,523,1047], [0.2,0.2,0.2,0.2,0.2,0,5]),
@@ -24,18 +32,9 @@ songs = [
 	]
 
 
-class LCD():
+class Music():
     def __init__(self, eventQueue):
-
-        # Configuration parameters
-        # I2C Address, Port, Enable pin, RW pin, RS pin, Data 4 pin, Data 5 pin, Data 6 pin, Data 7 pin, Backlight pin (optional)
-        self.lcd = i2c_lcd_smbus.i2c_lcd(0x27,1, 2, 1, 0, 4, 5, 6, 7, 3)
-
-        # If you want to disable the cursor, uncomment the following line
-        self.lcd.command(self.lcd.CMD_Display_Control | self.lcd.OPT_Enable_Display)
-        self.lcd.backLightOn()
-        self.isLighted=True
-	thread.start_new_thread(self.main, (eventQueue, ))
+        thread.start_new_thread(self.main, (eventQueue, ))
 
     def main( self, eventQueue ):
 	while (True):
@@ -52,10 +51,13 @@ class LCD():
         GPIO.setup(12, GPIO.OUT)
         pin = GPIO.PWM(12, 5000)  # channel=12 frequency=50Hz
         pin.start(0)
-        self.playSong(pin, song)
+        self.playNotes(pin, song)
             
+    def playNotes( self, pin, song ):
+        for note in zip( song[0], song[1]):
+            self.playNote(pin, note[0], note[1] )
 
-    def beep( self, pin, frequency, length ):
+    def playNote( self, pin, frequency, length ):
         if frequency>0:
             pin.ChangeDutyCycle(50)
             pin.ChangeFrequency( frequency )
@@ -63,21 +65,17 @@ class LCD():
         pin.ChangeDutyCycle(0)
         time.sleep( length/2)
 
-    def playSong( self, pin, song ):
-        for note in zip( song[0], song[1]):
-            self.beep(pin, note[0], note[1] )
-
 def main( ):
     from Queue import Queue
     q=Queue()
-    sc = LCD(q)
+    sc = Music(q)
     while True:
         q.put([const.EVENT_SONG1, "hello"])
-        time.sleep(20)
+        time.sleep(10)
         q.put([const.EVENT_SONG2,"" ])
         time.sleep(10)
         q.put([const.EVENT_SONG3, "hello"])
-        time.sleep(20)
+        time.sleep(10)
     
 if __name__ == '__main__':
     main()
